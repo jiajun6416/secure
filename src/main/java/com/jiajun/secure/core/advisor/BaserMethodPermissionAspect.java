07220c620c310072617fc39cfd052a9c4c656e34
jerry <jiajun6416@gmail.com>
2017-09-29 14:32:36 +0800

    添加权限logic判断, 允许权限进行与或操作

--- src/main/java/com/jiajun/secure/core/advisor/BaserMethodPermissionAspect.java
+++ src/main/java/com/jiajun/secure/core/advisor/BaserMethodPermissionAspect.java
@@ -1,5 +1,6 @@
 package com.jiajun.secure.core.advisor;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.aspectj.lang.JoinPoint;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 
+import com.jiajun.secure.core.annotation.Logical;
 import com.jiajun.secure.core.annotation.RequiresPermissions;
 import com.jiajun.secure.core.annotation.RequiresRoles;
 import com.jiajun.secure.core.exception.UnauthenticatedException;
@@ -32,12 +34,30 @@ public class BaserMethodPermissionAspect implements Ordered{
 		String method = joinPoint.getSignature().getName();
 		String[] roles = requiresRoles.value();
 		List<String> hasRoles = userService.hasRoles();
-		for (String role : roles) {
-			if(!hasRoles.contains(role)) {
-				System.out.println(String.format("拦截方法%s.%s,不具备%s角色", targetName, method,role));
-				throw new UnauthenticatedException("不具备"+role+"角色");
-			}
-			System.out.println(String.format("拦截方法%s.%s,具备%s角色", targetName, method,role));
+		Logical logical = requiresRoles.logical();
+		switch (logical) {
+			case OR:
+				boolean hasRole = false;
+				for (String role : roles) {
+					if (hasRoles.contains(role)) {
+						hasRole = true;
+						System.out.println(String.format("拦截方法%s.%s,具备%s角色", targetName, method, role));
+						break;
+					}
+				}
+				if (!hasRole) {
+					System.out.println(String.format("拦截方法%s.%s,不具备%s角色", targetName, method, roles));
+					throw new UnauthenticatedException("不具备" + roles + "角色之一");
+				}
+				break;
+			default:
+				for (String role : roles) {
+					if (!hasRoles.contains(role)) {
+						System.out.println(String.format("拦截方法%s.%s,不具备%s角色", targetName, method, role));
+						throw new UnauthenticatedException("不具备" + role + "角色");
+					}
+					System.out.println(String.format("拦截方法%s.%s,具备%s角色", targetName, method, role));
+				}
 		}
 	}
 	
@@ -45,14 +65,32 @@ public class BaserMethodPermissionAspect implements Ordered{
 	public void hasPermission(JoinPoint joinPoint, RequiresPermissions requiresPermissions) {
 		String targetName = joinPoint.getTarget().getClass().getName();
 		String method = joinPoint.getSignature().getName();
+		//具备的资源
+		List<String> hasPers = Arrays.asList("");
 		String[] permissions = requiresPermissions.value();
-		List<String> haspers = userService.hasPermissions();
-		for (String permission : permissions) {
-			if(!haspers.contains(permission)) {
-				System.out.println(String.format("拦截方法%s.%s,不具备%s资源", targetName, method,permission));
-				throw new UnauthenticatedException("不具备"+permission+"权限");
-			}
-			System.out.println(String.format("拦截方法%s.%s,具备%s资源", targetName, method,permission));
+		Logical logical = requiresPermissions.logical();
+		switch (logical) {
+			case OR:
+				boolean hasPermission = false;
+				for (String permission : permissions) {
+					if(hasPers.contains(permission)) {
+						hasPermission = true;
+						System.out.println(String.format("拦截方法%s.%s,具备%s资源", targetName, method, permission));
+						break;
+					}
+				}
+				if(!hasPermission) {
+					throw new UnauthenticatedException("不具备" + permissions + "权限之一");
+				}
+				break;
+			default:
+				for (String permission : permissions) {
+					if (!hasPers.contains(permission)) {
+						System.out.println(String.format("拦截方法%s.%s,不具备%s资源", targetName, method, permission));
+						throw new UnauthenticatedException("不具备" + permission + "权限");
+					}
+					System.out.println(String.format("拦截方法%s.%s,具备%s资源", targetName, method, permission));
+				}
 		}
 	}
 	
